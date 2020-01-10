c618cdd246759073ea8ecb8106a9792d1d1a4e2a
dhpradeep <dhpradeep25@gmail.com>
2020-01-10 14:52:47 +0545

    Design header

--- src/components/contacts/contacts.js
+++ src/components/contacts/contacts.js
@@ -1,9 +1,12 @@
 import React from 'react'
-import {View,Text} from 'react-native'
+import { View, Text } from 'react-native'
+
+import Header from '../../common/header'
 
 export default function Contact() {
     return (
         <View>
+            <Header title="Contact" />
             <Text>Contact default page setup</Text>
         </View>
     )
