37785d4d365e331d35ad54d6ef8ff247925d226b
dhpradeep <dhpradeep25@gmail.com>
2020-01-17 09:20:58 +0545

    Fix header problem

--- src/views/contacts/contacts.js
+++ src/views/contacts/contacts.js
@@ -1,15 +1,30 @@
-import React, { Component } from 'react'
-import { View, Text } from 'react-native'
+import React, {Component} from 'react';
+import {View, FlatList, Text} from 'react-native';
 
-import { Header } from '../../components/header'
+import {Header} from '../../components/header';
+import EmpItem from './employeeItem';
+const empList = require('../../services/empList.json');
 
 export default class Contact extends Component {
-    render() {
-        return (
-            <View>
-                <Header title="Contact" />
-                <Text>Contact default page setup</Text>
-            </View>
-        )
-    }
+
+  constructor(props) {
+    super(props);
+  }
+
+  _onItemClick = (id) => {
+    this.props.navigation.navigate('ContactDetails', {id})
+  }
+
+  render() {
+    return (
+      <View>
+         <Header title="Contact" />
+        <FlatList
+          data={empList}
+          renderItem={({item}) => <EmpItem onItemClick={() => this._onItemClick(item.id)} item={item} />}
+          keyExtractor={item => item.id}
+        />
+      </View>
+    );
+  }
 }
